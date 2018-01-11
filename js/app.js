6b9537f2e9ab8388ca964d45dea4ca9c2efde07a
Mike Murphy <mike@brainstormchaser.com>
2018-01-10 19:56:57 -0800

    language switching and visualization added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+// bitcoin widget event that pulls a live BTC price
 (function(b,i,t,C,O,I,N) {
     window.addEventListener('load',function() {
       if(b.getElementById(C))return;
@@ -6,6 +8,7 @@
     },false)
   })(document,'script','https://widgets.bitcoin.com/widget.js','btcwdgt');
 
+// page scrolling code and jquery to write live BTC price to widget
 (function($) {
   "use strict";
 
@@ -22,14 +25,33 @@
     }
   });
 
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
-
 })(jQuery);
+
+var countryCode = {
+  US: 'Welcome!',
+  FR: 'Bienvenue!',
+  IT: 'Benvenuto!',
+  MX: 'Bienvenido!'
+};
+
+$('.translate').click(function(){
+  var lang = $(this).attr('id').toUpperCase();
+  $('#welcome').text(countryCode[lang]);
+});
+
+$.ajax({
+  url: 'http://freegeoip.net/json/',
+  type: 'GET',
+  dataType: 'jsonp',
+  success: function(location) {
+    console.log(countryCode[location.country_code]);
+    $('#welcome').text(countryCode[location.country_code]);
+    if(location.country_code === 'US'){
+      // remove english button
+      //jquery to find id and .hide button
+      $('.')
+    } else if (location.country_code === 'FR') {
+      // remove french button 
+    }
+  }
+});
